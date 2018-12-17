4cc37e66e383f780dd055372e0545e6802844e4e
Sam Glerum <samuelglerum@hotmail.com>
2018-12-17 15:35:06 +0100

    added redirect when user goes to '/'

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
+// Express imports
 const express = require('express');
 const server = express();
+// Model imports
+const jsonModel = require('./models/response/JsonModel');
+// Constant declarations
 const port = process.env.PORT || 3000;
 
 server.get("/", (req, res) => {
-    res.send("Welcome to the a2_cm api");
+    res.redirect("/api");
+});
+
+server.get("/api", (req, res) => {
+    res.status(200).json(new jsonModel("/api", "GET", 200, "Welcome to the a2_cm api"));
 });
 
 
