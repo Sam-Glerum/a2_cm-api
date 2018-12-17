498c28775ac516dcaed5fa57f94d280ce12adae2
Sam Glerum <samuelglerum@hotmail.com>
2018-12-17 15:43:47 +0100

    setup first routes. added apiVersion to JSON response model

--- index.js
+++ index.js
@@ -4,14 +4,15 @@ const server = express();
 // Model imports
 const jsonModel = require('./models/response/JsonModel');
 // Constant declarations
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000
+const apiVersion = "v1";
 
 server.get("/", (req, res) => {
     res.redirect("/api");
 });
 
 server.get("/api", (req, res) => {
-    res.status(200).json(new jsonModel("/api", "GET", 200, "Welcome to the a2_cm api"));
+    res.status(200).json(new jsonModel("/api", "GET", 200, apiVersion, "Welcome to the a2_cm api"));
 });
 
 
--- models/response/JsonModel.js
+++ models/response/JsonModel.js
@@ -1,9 +1,10 @@
 class JsonResponseModel {
 
-    constructor(url, method, statusCode, message) {
+    constructor(url, method, statusCode, apiVersion, message) {
         this.url = url;
         this.method = method;
         this.statusCode = statusCode;
+        this.apiVersion = apiVersion;
         this.message = message;
     };
 }
