8c039771b054f13c41f16b1dfd63e6a7bc13324d
Sam Glerum <samuelglerum@hotmail.com>
2018-12-17 20:11:27 +0100

    added route that is accessed by all requests to check if a user is authenticated

--- routes/v1/authentication_routes_v1.js
+++ routes/v1/authentication_routes_v1.js
@@ -1,6 +1,27 @@
 // Express imports
 const express = require('express');
 const router = express.Router();
+// Authentication imports
+const authentication = require('../../authentication/authentication');
+
+// Route that is accessed by all requests to check if the user is authenticated to the server
+router.all(new RegExp("^(?!\/login$|\/register$).*"), (req, res, next) => {
+    // Get token from headers
+    const token = req.header('X-Access-Token');
+
+    authentication.decodeToken(token, (error, payload) => {
+        if (error) {
+            console.log('Authentication error: ' + error.message);
+
+            res.status((error.status || 401)).json("Not Authorised");
+        } else {
+            req.user = {
+                username: payload.sub
+            };
+            next();
+        }
+    })
+});
 
 // Login route
 router.get('/login', (req, res) => {
