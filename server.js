315c2b12fb9f911dc0e8bf90df8d374937d4fdc2
Sam Glerum <samuelglerum@hotmail.com>
2018-12-18 11:41:31 +0100

    fixed CORS Headers

--- server.js
+++ server.js
@@ -19,8 +19,13 @@ if (process.env.NODE_ENV !== 'production') {
 // Load body-parser to read the body from requests
 server.use(bodyparser.json());
 
-// Enable CORS headers
-server.use(cors());
+
+//CORS headers
+server.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', process.env.ALLOW_ORIGIN || 'http://localhost:4200');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, x-access-token');
+    res.setHeader('Access-Control-Allow-Credentials', true);
 
 const DATABASE_NAME = process.env.dbName;
 const dbUser = process.env.dbUser;
