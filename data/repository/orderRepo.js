df60d0fbff31d4f767deae785d26e95516c88b44
Sam Glerum <samuelglerum@hotmail.com>
2018-12-19 21:31:45 +0100

    created method getOrderById which retrieves a single order from the Orders table

--- data/repository/orderRepo.js
+++ data/repository/orderRepo.js
@@ -3,18 +3,36 @@ const sql = require('mssql');
 // Response model imports
 const jsonModel = require('../../models/response/JsonModel');
 
+const sqlRequest = new sql.Request();
+
 module.exports = class orderRepo {
+
+
 
     static async getAllOrders(res) {
         const reqUrl = '/api/orders';
         const httpMethod = 'GET';
 
-        const sqlRequest = new sql.Request();
-
         sqlRequest.query('select * from Orders', (error, recordSet) => {
             if (error) {
                 console.log(error);
                 res.status(404).json(new jsonModel(reqUrl, httpMethod, 404, "No orders found"));
+            }
+
+            res.status(200).json({
+                response: new jsonModel(reqUrl, httpMethod, 200, "Orders"),
+                orders: recordSet.recordset
+            })
+        })
+    }
+
+    static async getOrderByID(orderID, res) {
+        const reqUrl = '/api/orders/' + orderID;
+        const httpMethod = 'GET';
+
+        sqlRequest.query('select * from Orders where ID = ' + orderID, (error , recordSet) => {
+            if (error) {
+                console.log(error);
             }
 
             res.status(200).json({
