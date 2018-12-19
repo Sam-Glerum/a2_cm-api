6eff2bb8583e209a4d1c214ebea2b84b9d222dbd
Sam Glerum <samuelglerum@hotmail.com>
2018-12-19 18:32:09 +0100

    created method to retrieve all payments

--- data/repository/paymentRepo.js
+++ data/repository/paymentRepo.js
@@ -1,7 +1,24 @@
 // Database driver import
 const sql = require('mssql');
+// Response model import
+const jsonModel = require('../../models/response/JsonModel');
 
+export class PaymentRepo {
 
+    static getAllPayments(res) {
+        const reqUrl = '/api/payments';
+        const httpMethod = 'POST';
+
+        let sqlRequest = sql.Request();
+
+        sqlRequest.query('select * from Payment', (error, recordSet) => {
+            if (error) {
+                console.log(error);
+            }
+            res.status(200).json(recordSet);
+        })
+    }
+}
 
 
 
