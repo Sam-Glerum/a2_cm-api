736e81d93820e2a953c383bba546186ada5145b5
Sam Glerum <samuelglerum@hotmail.com>
2018-12-20 12:12:24 +0100

    added routes for getting all and getting a single alert from the SQL database

---
+++ routes/v1/alert_routes_v1.js
@@ -0,0 +1,14 @@
+// Express imports
+const express = require('express');
+const router = express.Router();
+// Repository imports
+const alertRepo = require('../../data/repository/alertRepo');
+
+router.get('/', (req, res) => {
+    alertRepo.getAllALerts('GET', res);
+});
+
+router.get('/:alertID', (req, res) => {
+    let alertID = req.params.alertID;
+    alertRepo.getAlertByID(alertID, 'GET', res);
+});
