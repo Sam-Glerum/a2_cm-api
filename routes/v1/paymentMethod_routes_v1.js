6861625614f7e402d5ca1e6826ea83932d3b35ed
Sam Glerum <samuelglerum@hotmail.com>
2018-12-20 21:21:23 +0100

    created routes for getting payment methods

---
+++ routes/v1/paymentMethod_routes_v1.js
@@ -0,0 +1,17 @@
+// Express imports
+const express = require('express');
+const router = express.Router();
+// Repository imports
+const paymentMethodRepo = require('../../data/repository/paymentMethodRepo');
+
+router.get('/', (req, res) => {
+    paymentMethodRepo.getAllPaymentMethods('GET', res);
+});
+
+router.get('/:paymentMethodName', (req, res) => {
+    let paymentMethodName = req.params.paymentMethodName;
+
+    paymentMethodRepo.getPaymentMethodByName(paymentMethodName, 'GET', res);
+});
+
+module.exports = router;
