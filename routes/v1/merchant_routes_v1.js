fd7b1bd4e5882217da6ceb0625a7b55cfb6234c4
Sam Glerum <samuelglerum@hotmail.com>
2019-01-09 18:03:27 +0100

    refactored merchant routes sql queries

--- routes/v1/merchant_routes_v1.js
+++ routes/v1/merchant_routes_v1.js
@@ -3,15 +3,30 @@ const express = require('express');
 const router = express.Router();
 // Repository imports
 const merchantRepo = require('../../data/repository/merchantRepo');
+const sqlRepo = require('../../data/repository/sqlRepo');
 
 router.get('/', (req, res) => {
-    merchantRepo.getAllMerchants('GET', res);
+
+    sqlRepo.getAllItemsFromSQL(
+        res,
+        '/api/merchants',
+        'GET',
+        'select * from Merchants',
+        'Merchants'
+    )
 });
 
 router.get('/:merchantID', (req, res) => {
     let merchantID = req.params.merchantID;
 
-    merchantRepo.getMerchantByID(merchantID, 'GET', res);
+    sqlRepo.getSingleItemFromSQL(
+        res,
+        '/api/merchants/' + merchantID,
+        'GET',
+        'select * from Merchants where ID = \'' + merchantID + '\'',
+        'Merchants',
+        merchantID
+    )
 });
 
 module.exports = router;
