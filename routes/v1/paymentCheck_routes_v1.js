996431021c6ccb15498abec81bf627fddfcdbe57
Sam Glerum <samuelglerum@hotmail.com>
2019-01-09 13:54:20 +0100

    fixed typo in paymentcheckrepo

--- routes/v1/paymentCheck_routes_v1.js
+++ routes/v1/paymentCheck_routes_v1.js
@@ -7,7 +7,7 @@ const paymentCheckRepo = require('../../data/repository/paymentCheckRepo');
 // Create merchant check
 router.post('/', (req, res) => {
     let paymentCheck = req.body;
-    paymentCheckRepo.createPaymentCheck(paymentCheck.amount, paymentCheck.category, paymentCheck.time, paymentCheck.paymentMethod, "POST", res);
+    paymentCheckRepo.createPaymentCheck(paymentCheck.amount, paymentCheck.currency, paymentCheck.time, paymentCheck.paymentMethod, "POST", res);
 });
 
 // Delete merchant check
