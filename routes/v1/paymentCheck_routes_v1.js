05ddeb9d1c4b48aa90640c417e58b0f3c9c46b12
Unknown <rick.voesenek@hotmail.com>
2019-01-10 13:16:16 +0100

    Payments API Read and Update
    Read endpoint creataed for payment
    Update endpoints created for payment

--- routes/v1/paymentCheck_routes_v1.js
+++ routes/v1/paymentCheck_routes_v1.js
@@ -4,13 +4,24 @@ const router = express.Router();
 // Repository imports
 const paymentCheckRepo = require('../../data/repository/paymentCheckRepo');
 
-// Create merchant check
+// Create payment check
 router.post('/', (req, res) => {
     let paymentCheck = req.body;
     paymentCheckRepo.createPaymentCheck(paymentCheck.amount, paymentCheck.currency, paymentCheck.time, paymentCheck.paymentMethod, "POST", res);
 });
 
-// Delete merchant check
+// Read payment check
+router.get('/', (req, res) => {
+    paymentCheckRepo.readPaymentCheck("GET", res);
+});
+
+// Update payment check
+router.put('/', (req, res) => {
+    let paymentCheck = req.body;
+    paymentCheckRepo.updatePaymentCheck(paymentCheck.checkId, paymentCheck.amount, paymentCheck.currency, paymentCheck.time, paymentCheck.paymentMethod, "PUT", res);
+});
+
+// Delete payment check
 router.delete('/:checkID', (req, res) => {
     let checkID = req.params.checkID;
     paymentCheckRepo.deletePaymentCheck(checkID, "DELETE", res);
