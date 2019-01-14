facff42603b2fec2e8f801cd95aa24342293e14d
Sam Glerum <samuelglerum@hotmail.com>
2019-01-14 14:12:57 +0100

    created repo for buyer checks

---
+++ data/repository/buyerCheckRepo.js
@@ -0,0 +1,8 @@
+// Response model imports
+const jsonModel = require('../../models/response/JsonModel');
+// Entity imports
+const buyerCheck = require('../schema/buyerCheck');
+
+module.exports = class BuyerCheckRepo {
+
+};
